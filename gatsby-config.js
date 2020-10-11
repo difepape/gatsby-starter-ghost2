0a86db892834a3f1a3e2e98f9cbccf47a545b704
difepape <51058420+difepape@users.noreply.github.com>
2020-10-11 18:27:15 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ try {
     }
 }
 
-if (process.env.NODE_ENV === `production` && config.siteUrl === `http://localhost:8000` && !process.env.SITEURL) {
+if (process.env.NODE_ENV === `production` && config.siteUrl === `http://34.94.245.240:8000` && !process.env.SITEURL) {
     throw new Error(`siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`) // eslint-disable-line
 }
 
